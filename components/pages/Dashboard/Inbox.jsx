66b217223b6997239eabda26df98be54becd6500
Rohit Rana <ranarohitgrd@gmail.com>
2025-06-16 20:18:41 +0530

    font size change in home page

--- components/pages/Dashboard/Inbox.jsx
+++ components/pages/Dashboard/Inbox.jsx
@@ -44,7 +44,7 @@ const InboxTable = () => {
         <table className="text-primary w-full min-w-[700px] border-collapse">
           <thead>
             <tr className="bg-[#386B67] text-gray-700">
-              <th className="text-md abril text-light py-3 pl-4 text-left tracking-wider md:pl-9">
+              <th className="md:text-md abril text-light py-3 pl-4 text-left text-sm tracking-wider md:pl-9">
                 Inbox
                 <i className="ri-delete-bin-6-line cursor-pointer pl-8"></i>
                 <i className="ri-star-line cursor-pointer px-3"></i>
@@ -64,7 +64,7 @@ const InboxTable = () => {
                   Unread
                 </button>
               </th>
-              <th className="cursor-pointer py-3 text-left text-sm font-medium">
+              <th className="cursor-pointer px-4 py-3 text-left text-sm font-medium">
                 <button className="text-light"> All View</button>
               </th>
             </tr>
--- components/pages/Dashboard/InboxDiv.jsx
+++ components/pages/Dashboard/InboxDiv.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client" 
 
 const reviewsData = [
   {
@@ -43,7 +43,7 @@ const ReviewCard = ({ name, date, review, rating ,img}) => {
     <div className="bg-white px-6 py-2">
    <div className="flex items-center gap-2">
    <img src={img} alt="Ellipse" className="rounded-full"/>
-       <h3 className="text-hcolor text-md abril">{name}</h3>
+       <h3 className="text-hcolor md:text-md text-sm abril">{name}</h3>
    </div>
       <div className="flex items-center gap-2">
         <p className="flex text-md text-[#2CB24B]">
@@ -69,7 +69,7 @@ const ReviewCard2 = ({ name2, date2, review2, rating2 ,img2}) => {
     <div className="bg-white px-6 py-2">
    <div className="flex items-center gap-2">
    <img src={img2} alt="Ellipse" className="rounded-full"/>
-       <h3 className="text-hcolor text-md abril ">{name2}</h3>
+       <h3 className="text-hcolor md:text-md  text-sm abril ">{name2}</h3>
    </div>
       <div className="flex items-center gap-2">
         <p className="flex text-md text-[#2CB24B]">
@@ -98,7 +98,7 @@ const InboxDiv = () => {
     <section className="mt-12 flex md:flex-row flex-col md:w-9/13 md:gap-5 gap-7">
       {/* Larger Container */}
       <div className="flex-[4] rounded-xl border-x border-gray-100">
-        <h4 className="text-md text-light rounded-t-xl bg-[#386B67] px-6 py-3  abril">
+        <h4 className="md:text-md text-sm text-light rounded-t-xl bg-[#386B67] px-6 py-3  abril">
           Patient Reviews for Doctors
         </h4>
         {allReviews.map((review, index) => (
@@ -108,7 +108,7 @@ const InboxDiv = () => {
 
       {/* Smaller Container */}
       <div className="flex-[3] rounded-xl border-x border-gray-100">
-        <h4 className="text-md text-light rounded-t-xl bg-[#4D9691] px-6 py-3  abril">
+        <h4 className="md:text-md text-sm text-light rounded-t-xl bg-[#4D9691] px-6 py-3  abril">
           CEO Office Feedback
         </h4>
         {ceoReviews.map((review, index) => (
